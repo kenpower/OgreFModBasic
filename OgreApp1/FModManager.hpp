c6451e2a649746ea60a8ca3972d82e1e8f2c8029
Ken Power <github@kenpower.com>
2013-11-11 09:01:33 +0000

    First Commit
    Incorporated FMOD into Simple Ogre Project

---
+++ OgreApp1/FModManager.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#pragma comment(lib,"fmodex_vc.lib") 
+#include "fmod.hpp"
+#include <iostream>
+ 
+class FModManager
+{
+public:
+	static FMOD::System* System();
+ 
+private:
+	static FMOD::System *system;
+ 
+};
